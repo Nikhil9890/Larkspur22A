6e03a2b18c8797d87dc717bd1547ef3dd41d12e6
nsdeshmukhnd <nsdeshmukh78@gmail.com>
2023-04-12 13:33:06 +0530

    jenkins update

--- src/main/java/com/TestingShastra/Larkspur22A_FrameWorkmainDemo/WaitDemo.java
+++ src/main/java/com/TestingShastra/Larkspur22A_FrameWorkmainDemo/WaitDemo.java
@@ -10,12 +10,10 @@ import org.openqa.selenium.support.ui.FluentWait;
 public class WaitDemo {
 	
 	public static void main(String[] args) {
-		
 		WebDriver driver=new ChromeDriver();
 		FluentWait waitFor= new FluentWait(driver);
 		waitFor.withTimeout(Duration.ofSeconds(10));
 		waitFor.pollingEvery(Duration.ofSeconds(1));
 		waitFor.ignoring(NoSuchElementException.class);
 	}
-
 }
